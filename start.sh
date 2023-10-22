d6ba308fe917f8a48bb769b51eb0fa364ca9c911
Bumb1ebe <86529395+Bum81ebe@users.noreply.github.com>
2023-10-22 16:57:34 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-sudo apt install dos2unix | wget https://raw.githubusercontent.com/Bum81ebe/Project-X/main/install_script.sh && dos2unix install_script.sh && sudo bash install_script.sh
+sudo apt-get update | sudo apt install dos2unix | wget https://raw.githubusercontent.com/Bum81ebe/Project-X/main/install_script.sh && dos2unix install_script.sh && sudo bash install_script.sh
